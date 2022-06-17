5e13c61450bc9f07da9f7f5f9e2ce82f2fbbe055
Naina070806 <naina.sardana06@gmail.com>
2022-06-17 16:03:09 +0530

    added all the info, still have a little left for australia so if one of you could check that :)

--- NEWFinalProject/AustraliaViewController.swift
+++ NEWFinalProject/AustraliaViewController.swift
@@ -12,7 +12,7 @@ class AustraliaViewController: UIViewController {
     @IBOutlet weak var label4: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        label4.text = "bootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcatsbootsandcats"
+        label4.text = "Main Factor Attributed To The Issue: Permanent removal of native vegetation. \n \n \n Why Is The Issue Still Prevalent? : \n \n wind erosion, water erosion, soil fertility decline, soil salinity, soil salinity and soil acidification. \n \n \n Positive Developments: \n \n 1) Laser grading of irrigation land. \n 2) Reduced use of pesti- cides and trash retention. \n \n \n Companies And NGOs To Support \n \n 1) . \n 2) . \n 3) .  \n 4) . \n \n \n What Can You Do To Help? \n \n 1) . \n 2) . \n 3) ."
         // Do any additional setup after loading the view.
     }
     
